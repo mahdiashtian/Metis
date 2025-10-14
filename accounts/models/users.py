d5025c7847a92b43c110cae0a239770d7631f8ef
Mahdi <mahdiashtian.mo@gmail.com>
2025-10-14 14:50:21 +0330

    feat: add fields [code - image - verify_phone_number] to user

--- accounts/models/users.py
+++ accounts/models/users.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from redis.commands.helpers import random_string
 
 from utils.type import RoleChoices
 
 
 class User(AbstractUser):
-
-
     role = models.CharField(choices=
                             RoleChoices.choices, default=RoleChoices.EMPLOYEE.value)
     phone_number = models.CharField(max_length=13, unique=True)
+    verify_phone_number = models.BooleanField(default=False)
+    code = models.CharField(default=random_string, max_length=10, unique=True, editable=False)
+    image = models.ImageField(null=True, blank=True)
+
     is_superuser = None
     is_staff = None
