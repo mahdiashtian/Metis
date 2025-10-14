5e28b260c649edaeeef47659a425da011fac26bd
Mahdi <mahdiashtian.mo@gmail.com>
2025-10-14 14:51:39 +0330

    feat: remove refactor: restructure project architecture for better organization and maintainability

---
+++ accounts/api/me/me_api.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from rest_framework.generics import UpdateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from accounts.api.me.serializers import ReadMeSerializer, MeWriteSerializer
+from library.exceptions import UserNotFound
+
+User = get_user_model()
+
+
+class MeApi(UpdateAPIView, RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    http_method_names = ["get", "patch", "put"]
+    lookup_field = "id"
+
+    def get_object(self):
+        user: User = self.queryset.filter(id=self.request.user.id).first()
+        if user is None:
+            raise UserNotFound
+        return user
+
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        match self.request.method.lower():
+            case "get":
+                serializer_class = ReadMeSerializer
+            case "patch" | "pit":
+                serializer_class = MeWriteSerializer
+        return serializer_class
---
+++ accounts/api/me/serializers/write.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from library.exceptions.conflict import PhoneAlreadyExists, EmailAlreadyExists
+
+User = get_user_model()
+
+
+class MeWriteSerializer(serializers.Serializer):
+    first_name = serializers.CharField(required=False, allow_blank=True, max_length=150)
+    last_name = serializers.CharField(required=False, allow_blank=True, max_length=150)
+    phone_number = serializers.CharField(required=False, allow_blank=True, max_length=13)
+    email = serializers.EmailField(required=False, allow_blank=True)
+    image = serializers.ImageField(required=False, allow_null=True)
+
+    def validate_phone_number(self, value):
+        if User.objects.exclude(pk=self.instance.pk).filter(phone_number=value).exists():
+            raise PhoneAlreadyExists
+        return value
+
+    def validate_email(self, value):
+        if User.objects.exclude(pk=self.instance.pk).filter(email=value).exists():
+            raise EmailAlreadyExists
+        return value
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save(update_fields=[
+            "first_name",
+            "last_name",
+            "phone_number",
+            "verify_phone_number",
+            "email",
+            "image",
+        ])
+        return instance
